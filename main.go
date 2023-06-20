461552083ccfa4ac4c6099102d8c35b8699fa13b
ulngollm <ulngollm@gmail.com>
2023-06-20 11:33:21 +0300

    add basic sqlite example

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+
+type Transaction struct {
+	Id int64
+	Desc string
+	Sum int64
+}
 
 func main() {
-	fmt.Println("Hello")
+	db, _ := sql.Open("sqlite", "file:app.db")
+
+	rows, _ := db.Query("select * from transactions")
+	
+	if _, err := db.Exec(`
+		drop table if exists transactions;
+		CREATE TABLE transactions
+   		    (id INTEGER PRIMARY KEY,
+   		     desc TEXT DEFAULT '',
+   			sum INTEGER);
+		`); err != nil {
+			fmt.Println(err)
+		}
+
+	var transactions []Transaction
+	for rows.Next(){
+		var transaction Transaction
+		if err := rows.Scan(&transaction.Id, &transaction.Desc, &transaction.Sum); err != nil {
+			fmt.Println(err)
+		}
+		transactions = append(transactions, transaction)
+	}
+
+	fmt.Printf("%v", transactions)
+
+	if err := db.Close(); err != nil {
+		fmt.Println(err)
+	}
+
 }
